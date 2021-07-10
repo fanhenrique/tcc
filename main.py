a0ee441a890af99b24b248e6d5ce4a7eaa0d49c6
henrique fan <fanhenrique@gmai.com>
2021-07-10 16:07:19 -0300

    gera grafo

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import matplotlib
+import networkx as nx
+import matplotlib.pyplot as plt
+import pandas as pd 
+
 import argparse
 import logging
+
 
 DEFAULT_LOG_LEVEL = logging.INFO
 TIME_FORMAT = '%Y-%m-%d,%H:%M:%S'
@@ -34,21 +40,46 @@ def main():
 		logging.basicConfig(format='%(asctime)s.%(msecs)03d %(message)s', datefmt=TIME_FORMAT, level=args.log)
 
 	traces = []
+
+	nodes_peers = []
+	nodes_monitors = [] 
+
 	with open('data/data', 'r') as file:
 		
 		file.readline() #ignora cabeçalho 
 		
 		# for line in file:
-		for i in range(0, 100000):
+		for i in range(0, 1000000):
 			
 			# window, time, peer1, peer2, monitor1, monitor2 = line.split(' ')			
 			window, time, peer1, peer2, monitor1, monitor2 = file.readline().split(' ')
 			
-			print(window, time, peer1, peer2, monitor1, monitor2)
+			nodes_peers.append(peer1)
 			
+			nodes_monitors.append(monitor1)
+			
+			# print(window, time, peer1, peer2, monitor1, monitor2)
 
 			# traces.append(Trace(window, time, peer1, peer2, monitor1, monitor2))
 			# traces[i].printTrace()
 
+
+	graph = nx.Graph()
+	pos = nx.spring_layout(graph)
+
+	graph.add_nodes_from(nodes_peers, nodetype='red')
+	graph.add_nodes_from(nodes_monitors, nodetype='blue')
+
+	print(graph.nodes())
+
+	graph.add_edges_from(list(zip(nodes_peers, nodes_monitors)))
+
+	print(graph.edges())
+
+	colors = [u[1] for u in graph.nodes(data="nodetype")]
+	nx.draw(graph, with_labels=True, node_color=colors)
+
+	plt.show()
+	
 if __name__ == '__main__':
 	main()
