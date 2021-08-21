b3d6119ad5ccbbdcf0bb578b5def49e9d65a23c8
henrique fan <fanhenrique@gmai.com>
2021-08-21 00:13:56 -0300

    update

--- create_toy_case.py
+++ create_toy_case.py
@@ -1,9 +1,17 @@
-def readFile(file, n):
+import argparse
+import logging
+
+DEFAULT_LOG_LEVEL = logging.INFO
+TIME_FORMAT = '%Y-%m-%d, %H:%M:%S'
+
+def readFile(file):
 	epochs, trakers, monitors = [], [], []
 	with open(file, 'r') as file:
 		file.readline() #ignora cabeçalho 
 		for line in file:
-			line_split = line.split()			
+
+			line_split = line.split()
+			
 			try:
 				epochs.append(float(line_split[0]))
 			except:
@@ -22,7 +30,60 @@ def readFile(file, n):
 				epochs.pop()
 				trakers.pop()
 				continue
-	return epochs, trakers, monitors
+			try:
+				peer_list = []
+				peer_list.append(line_split[18].split('{')[1].split(':')[0])
+				i = 20
+				while True:
+					try:
+						peer_list.append(line_split[i].split("'")[1].split(':')[0])
+					except IndexError:
+						break
+					i+=2
+			except:
+				print(line)
+				epochs.pop()
+				trakers.pop()
+				monitors.pop()	
+
+	return epochs, trakers, monitors, peer_list
+
+
+def cal_windows(epoch, number_windows):
+	
+	time_min = []
+	windows = []
+
+
+	w_previous = 0
+	counter_windows = 0
+
+	for e in epoch:		
+		if counter_windows < number_windows:
+
+			tm = (e - epoch[0]) / 60	
+			w = math.trunc(tm / WINDOWS_LEN)
+
+			if w_previous != w:
+				counter_windows+=1	
+		
+			time_min.append(tm)
+			windows.append(w)
+			w_previous = w
+		else:
+			break
+	
+	windows_index_range = []
+	break0 = 0
+	for i in range(len(windows)-1):
+		if windows[i] != windows[i+1]:
+			break1 = i
+			windows_index_range.append((break0, break1))
+			break0 = break1+1
+
+
+	return time_min, windows, windows_index_range	
+
 
 
 def main():
@@ -42,7 +103,14 @@ def main():
 		logging.basicConfig(format='%(asctime)s.%(msecs)03d: %(message)s', datefmt=TIME_FORMAT, level=args.log)
 
 
-	= readFile(args.file)
+	logging.info('reading file ...')
+	epochs, trakers, monitors =  readFile(args.file)
+
+	logging.info('calculating windows ...')
+	time_min, windows, windows_index_range = cal_windows(epochs, args.numberwindows)
+
+	
+
 
 
 if __name__ == '__main__':
