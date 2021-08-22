518aa00c12fe011827ac44f6eb83dfc65f5145e5
henrique fan <fanhenrique@gmai.com>
2021-08-22 15:19:40 -0300

    update

--- create_toy_case.py
+++ create_toy_case.py
@@ -1,5 +1,7 @@
 import math
 
+import utils
+
 import argparse
 import logging
 
@@ -10,15 +12,6 @@ WINDOWS_LEN = 15
 TRACKER = 'TRACKER'
 MONITOR = 'MONITOR'
 PEER = 'PEER'
-
-hash_count_tracker = 1
-hash_table_tracker = {}
-
-hash_count_monitor = 1
-hash_table_monitor = {}
-
-hash_count_peer = 1
-hash_table_peer = {}
 
 
 def readFile(file):
@@ -66,88 +59,6 @@ def readFile(file):
 
 	return epochs, trakers, monitors, peer_lists
 
-
-def cal_windows(epoch, number_windows):
-	
-	time_min = []
-	windows = []
-
-
-	w_previous = 0
-	counter_windows = 0
-
-	for e in epoch:		
-		if counter_windows < number_windows:
-
-			tm = (e - epoch[0]) / 60	
-			w = math.trunc(tm / WINDOWS_LEN)
-
-			if w_previous != w:
-				counter_windows+=1	
-		
-			time_min.append(tm)
-			windows.append(w)
-			w_previous = w
-		else:
-			break
-	
-	windows_index_range = []
-	break0 = 0
-	for i in range(len(windows)-1):
-		if windows[i] != windows[i+1]:
-			break1 = i
-			windows_index_range.append((break0, break1))
-			break0 = break1+1
-
-
-	return time_min, windows, windows_index_range	
-
-
-
-def my_hash_tracker(tracker_str):
-
-	global hash_table_tracker
-	global hash_count_tracker
-
-	my_hash_tracker_value = hash_table_tracker.get(tracker_str)
-
-	if my_hash_tracker_value is None:
-		my_hash_tracker_value = hash_count_tracker
-		hash_table_tracker[tracker_str] = my_hash_tracker_value
-		hash_count_tracker += 1
-
-	return my_hash_tracker_value
-
-
-def my_hash_monitor(monitor_str):
-
-	global hash_table_monitor
-	global hash_count_monitor
-
-	my_hash_monitor_value = hash_table_monitor.get(monitor_str)
-
-	if my_hash_monitor_value is None:
-		my_hash_monitor_value = hash_count_monitor
-		hash_table_monitor[monitor_str] = my_hash_monitor_value
-		hash_count_monitor += 1
-
-	return my_hash_monitor_value
-
-def my_hash_peer(peer_str):
-
-	global hash_table_peer
-	global hash_count_peer
-
-	my_hash_peer_value = hash_table_peer.get(peer_str)
-
-	if my_hash_peer_value is None:
-		my_hash_peer_value = hash_count_peer
-		hash_table_peer[peer_str] = my_hash_peer_value
-		hash_count_peer += 1
-
-	return my_hash_peer_value
-
-
 def main():
 
 	parser = argparse.ArgumentParser(description='Create toy case')
@@ -166,21 +77,12 @@ def main():
 	else:
 		logging.basicConfig(format='%(asctime)s.%(msecs)03d: %(message)s', datefmt=TIME_FORMAT, level=args.log)
 
-	global hash_table_tracker
-	global hash_count_tracker
-
-	global hash_table_monitor
-	global hash_count_monitor
-
-	global hash_table_peer
-	global hash_count_peer
-
 
 	logging.info('reading file ...')
 	epochs, trakers, monitors, peer_lists =  readFile(args.file)
 
 	logging.info('calculating windows ...')
-	time_min, windows, windows_index_range = cal_windows(epochs, args.numberwindows)
+	time_min, windows, windows_index_range = utils.cal_windows(epochs, args.numberwindows)
 
 
 	# Label pra os vertices
--- graph_trackers_monitors.py
+++ graph_trackers_monitors.py
@@ -93,70 +93,8 @@ def show_graph(graph):
 
 	plt.show()
 
-def my_hash_tracker(tracker_str):
-
-	global hash_table_tracker
-	global hash_count_tracker
-
-	my_hash_tracker_value = hash_table_tracker.get(tracker_str)
-
-	if my_hash_tracker_value is None:
-		my_hash_tracker_value = hash_count_tracker
-		hash_table_tracker[tracker_str] = my_hash_tracker_value
-		hash_count_tracker += 1
-
-	return my_hash_tracker_value
 
 
-def my_hash_monitor(monitor_str):
-
-	global hash_table_monitor
-	global hash_count_monitor
-
-	my_hash_monitor_value = hash_table_monitor.get(monitor_str)
-
-	if my_hash_monitor_value is None:
-		my_hash_monitor_value = hash_count_monitor
-		hash_table_monitor[monitor_str] = my_hash_monitor_value
-		hash_count_monitor += 1
-
-	return my_hash_monitor_value
-
-
-def cal_windows(epoch, number_windows):
-	
-	time_min = []
-	windows = []
-
-
-	w_previous = 0
-	counter_windows = 0
-
-	for e in epoch:		
-		if counter_windows < number_windows:
-
-			tm = (e - epoch[0]) / 60	
-			w = math.trunc(tm / WINDOWS_LEN)
-
-			if w_previous != w:
-				counter_windows+=1	
-		
-			time_min.append(tm)
-			windows.append(w)
-			w_previous = w
-		else:
-			break
-	
-	windows_index_range = []
-	break0 = 0
-	for i in range(len(windows)-1):
-		if windows[i] != windows[i+1]:
-			break1 = i
-			windows_index_range.append((break0, break1))
-			break0 = break1+1
-
-
-	return time_min, windows, windows_index_range	
 
 def save_graph(graph, g):
 	# print(graph.nodes.data())
@@ -188,7 +126,7 @@ def main():
 	# REMOVER DEPOIS (apenas pra rodar com um arquivo menor)
 	# parser.add_argument('--numberlines', '-n', help='number lines', default=0, type=int) 
 	
-	parser.add_argument('--numberwindows', '-w', help='number windows', default=0, type=int) 
+	parser.add_argument('--numberwindows', '-w', help='number windows', default=1, type=int) 
 
 	help_msg = "Logging level (INFO=%d DEBUG=%d)" % (logging.INFO, logging.DEBUG)
 	parser.add_argument("--log", "-l", help=help_msg, default=DEFAULT_LOG_LEVEL, type=int)
