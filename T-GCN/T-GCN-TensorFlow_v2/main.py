c9080921fd20c80d8feb1301306a4791401c61e0
henrique fan <fanhenrique@gmai.com>
2021-08-23 14:25:43 -0300

    update

--- T-GCN/T-GCN-TensorFlow_v2/main.py
+++ T-GCN/T-GCN-TensorFlow_v2/main.py
@@ -18,6 +18,8 @@ import time
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+
 time_start = time.time()
 ###### Settings ######
 flags = tf.compat.v1.app.flags
