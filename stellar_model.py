985b590b1eaf68181594258324d153b244341fb9
Henrique Fan <fanhenrique@gmail.com>
2022-06-11 02:50:36 -0300

    update

--- stellar_model.py
+++ stellar_model.py
@@ -99,8 +99,8 @@ def plot_validation(train, validation, path_plots, name, title, ylabel):
 
 
 def main():
-    
-    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
     parser = argparse.ArgumentParser(description='gcn-lstm')
 
